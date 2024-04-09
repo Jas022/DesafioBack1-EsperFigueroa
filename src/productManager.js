286fdb88b16b59fa38131eef3619223d52e52450
Jasmin Esper <jasmin.09-@live.com.ar>
2024-04-09 00:30:12 -0300

    correccion

--- src/productManager.js
+++ src/productManager.js
@@ -1,31 +1,95 @@
-import express from "express";
-import { ProductManager } from "./productManager.js";
+const fs = require("fs");
+class ProductManager {
+  // #products;
+  #path;
 
-const app = express();
-const productManager = new ProductManager("./data/products.json");
-
-app.get("/products", async (req, res) => {
-  const result = await productManager.getProducts();
-  const limit = req.query.limit;
-  if (typeof result === "string") {
-    const error = result.split("");
-    return res
-      .status(parseInt(error[0].slice(1, 4)))
-      .json({ error: result.slice(6) });
+  constructor(path) {
+    this.#path = path;
+    this.#init();
   }
-  res.status(200).json({ payload: result.slice(0, limit) });
-});
 
-app.get("/products/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
-  const result = await productManager.getProductById(id);
-  if (typeof result === "string") {
-    const error = result.split("");
-    return res
-      .status(parseInt(error[0].slice(1, 4)))
-      .json({ error: result.slice(6) });
+  async #init() {
+    if (!fs.existsSync(this.#path)) {
+      await fs.promises.writeFile(this.#path, JSON.stringify([], null, 2));
+    }
   }
-  res.status(200).json({ payload: result });
-});
+  #getNextID(products) {
+    return products.length === 0 ? 1 : products[products.length - 1].id + 1;
+  }
 
-app.listen(8080, () => console.log("Server Up"));
+  async addProduct(product) {
+    if (
+      !product.title ||
+      !product.description ||
+      !product.price ||
+      !product.thumbnail ||
+      !product.code ||
+      !product.stock
+    )
+      return "[ERR] Required fields missing";
+    if (!fs.existsSync(this.#path)) return "[ERR] DB File does not exists";
+    let data = await fs.promises.readFile(this.#path, "utf-8");
+    let products = JSON.parse(data);
+    const found = products.find((item) => item.code === product.code);
+    if (found) {
+      return "[ERR] Code already exists";
+    }
+    const productToAdd = { id: this.#getNextID(products), ...product };
+    products.push(productToAdd);
+    await fs.promises.writeFile(this.#path, JSON.stringify(products, null, 2));
+    return productToAdd;
+  }
+
+  async getProducts() {
+    if (!fs.existsSync(this.#path)) return "[ERR] DB File does not exists";
+    let data = await fs.promises.readFile(this.#path, "utf-8");
+    const products = JSON.parse(data);
+    return products;
+  }
+
+  async getProductById(id) {
+    if (!fs.existsSync(this.#path)) return "[ERR] DB File does not exists";
+    let data = await fs.promises.readFile(this.#path, "utf-8");
+    let products = JSON.parse(data);
+    let product = products.find((item) => item.id === id);
+    if (!product) return "[ERR] NOt found";
+    return product;
+  }
+  async updateProduct(id, updateProduct) {
+    if (!fs.existsSync(this.#path)) return "[ERR] DB File does not exists";
+    let isFound = false;
+    let data = await fs.promises.readFile(this.#path, "utf-8");
+    let products = JSON.parse(data);
+    let newProducts = products.map((item) => {
+      if (item.id === id) {
+        isFound = true;
+        return {
+          ...item,
+          ...updateProduct,
+        };
+      } else return item;
+    });
+    if (!isFound) return "[ERR] DB File does not exists";
+    await fs.promises.writeFile(
+      this.#path,
+      JSON.stringify(newProducts, null, 2)
+    );
+    return newProducts.find((item) => item.id === id);
+  }
+  async deleteProduct(id) {
+    if (!fs.existsSync(this.#path)) return "[ERR] DB File does not exists";
+    let isFound = false;
+    let data = await fs.promises.readFile(this.#path, "utf-8");
+    let products = JSON.parse(data);
+    let newProducts = products.filter((item) => item.id !== id);
+    if (!isFound) return "[ERR] DB Product does not exists";
+    await fs.promises.writeFile(
+      this.#path,
+      JSON.stringify(newProducts, null, 2)
+    );
+    return newProducts;
+  }
+}
+
+// export default ProductManager;
+module.exports = ProductManager;
